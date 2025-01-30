9eb16e8f58a29945ae3e878b8fe99c42edf21f19
TomasiakM <marcintomasiak00@gmail.com>
2025-01-30 20:54:14 +0100

    Add semantic UI & update default layout

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,14 +1,21 @@
 import { Outlet, Link  } from 'react-router'
+import { Container, Header } from 'semantic-ui-react'
 
 function DefaultLayout() {
 
   return (
     <div>
-      <header>
-        <Link to="/">Rick and Morty</Link>
+      <header style={{ backgroundColor: '#b5cc18', padding: '10px 0', marginBottom: '0px' }}>
+        <Container>
+          <Header as='h1'>
+            <Link to="/" style={{ color: '#111111'}}>Rick and Morty</Link>
+          </Header>
+        </Container>
       </header>
       <main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </main>
     </div>
   )
